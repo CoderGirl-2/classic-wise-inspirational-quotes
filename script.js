629d5b3e2f1c6f337c04f14da64aaa71e0844ecc
codergirl <62530007+CoderGirl-2@users.noreply.github.com>
2025-09-30 19:55:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3SKLZKXGRZ');
+
+
+
+function copyText() {
+  const textToCopy = document.getElementById("myText").innerText;
+  navigator.clipboard.writeText(textToCopy)
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
