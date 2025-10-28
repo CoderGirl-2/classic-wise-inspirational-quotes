eebfb5482ed4333016a94ae7ec092f241e206aa8
codergirl <62530007+CoderGirl-2@users.noreply.github.com>
2025-10-28 05:54:49 -0500

    Refactor click event to toggle content display

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var contenthome = this.nextElementSibling;
+    if (contenthome.style.display === "block") {
+      contenthome.style.display = "none";
     } else {
-      content.style.display = "block";
+      contenthome.style.display = "block";
     }
   });
 
