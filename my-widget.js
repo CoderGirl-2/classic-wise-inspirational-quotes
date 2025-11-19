0a91c3bca7f7d2ebb13238186f296c65ba30c235
Classic Quotes from Classic Books <62530007+CoderGirl-2@users.noreply.github.com>
2025-11-19 04:22:02 -0600

    Update my-widget.js

--- my-widget.js
+++ my-widget.js
@@ -12,43 +12,6 @@ class MySimpleWidget extends HTMLElement {
         this.shadowRoot.innerHTML = `
         
 
-
-function copyText() {
-  const textToCopy = document.getElementById("myText").innerText;
-  navigator.clipboard.writeText(textToCopy)
-}
-
-
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
-var coll = document.getElementsByClassName("minicollapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var minicontent = this.nextElementSibling;
-    if (minicontent.style.display === "block") {
-      minicontent.style.display = "none";
-    } else {
-      minicontent.style.display = "block";
-    }
-  });
-}
 		
             <div class="widget-container">
                   <h3>Quote Directory</h3>
