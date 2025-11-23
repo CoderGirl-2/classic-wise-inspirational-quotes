aaab572b02c868ffc994eaa42bf3d3b1722315f1
Classic Quotes from Classic Books <62530007+CoderGirl-2@users.noreply.github.com>
2025-11-22 18:34:59 -0600

    Update my-widget.js

--- my-widget.js
+++ my-widget.js
@@ -30,53 +30,44 @@ class MySimpleWidget extends HTMLElement {
                 }
             </style>
             
-           
 
-	<button type="button" class="minicollapsible"><h3>Fahrenheit 451- Ray Bradbury▼</h3></button>
+		   <a href="https://sentry.io/answers/">
+  <button>Answers by Sentry</button>
+</a>
+
+
+	<button type="button" class="minicollapsible"><h3>Ray Bradbury▼</h3></button>
 <div class="minicontent">
 	<ul>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451-Building and Burning">Building and Burning</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451-Leaving a Legacy">Leaving a Legacy</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451-Literary Quality">Literary Quality</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451-Man doesn't give up">Man doesn't give up</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451>Fahrenheit 451</a></li>
 	</ul>
 </div>
 	
-<button type="button" class="minicollapsible"><h3>The Great Divorce- C.S Lewis▼</h3></button>
+<button type="button" class="minicollapsible"><h3>C.S Lewis▼</h3></button>
 <div class="minicontent">
 	<ul>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The%20Great%20Divorce-God%20doesn't%20need%20you">God doesn't need you</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce-Leaning on God">Leaning on God</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce-The Size of Bad vs Good">The Size of Bad vs Good</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce-Temporal Suffering">Temporal Suffering</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce-Thy will be done">Thy Will be done</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce">The Great Divorce</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Letters">Screwtape Letters</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Proposes a Toast">Screwtape Proposes a Toast</a></li>
 	</ul>
 </div>
 
-<button type="button" class="minicollapsible"><h3>Heart of Darkness- Joseph Conrad▼</h3></button>
+<button type="button" class="minicollapsible"><h3>Joseph Conrad▼</h3></button>
 <div class="minicontent">
 	<ul>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Heart of Darkness-Idealism">Idealism</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Heart of Darkness-Reading as a Friend">Reading as a Friend</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Heart of Darkness">Heart of Darkness</a></li>
 	</ul>
 </div>
 	
-		<button type="button" class="minicollapsible"><h3>Screwtape Letters- C.S Lewis▼</h3></button>
+		<button type="button" class="minicollapsible"><h3>Aldous Huxley▼</h3></button>
 <div class="minicontent">
 	<ul>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Letters-Difficulty of Perseverence">Difficulty of Perseverence</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Letters-Historical Point of View">Historical Point of View</a></li>
+		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Brave New World">Brave New World</a></li>
 	</ul>
 </div>
 
 
-		<button type="button" class="minicollapsible"><h3>Screwtape Proposes a Toast- C.S Lewis▼</h3></button>
-<div class="minicontent">
-	<ul>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Proposes a Toast-Mediocratization of Education">Mediocratization of Education</a></li>
-		<li><a target='_top'  href="https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Proposes a Toast-Mediocratization of Excellence">Mediocratization of Excellence</a></li>
-	</ul>
-</div>
+		
                 </div>
                 
                 <slot></slot> 
