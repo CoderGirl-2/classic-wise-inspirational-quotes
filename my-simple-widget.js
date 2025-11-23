f633481c4cd1be93288064d388474235061b6fc1
Classic Quotes from Classic Books <62530007+CoderGirl-2@users.noreply.github.com>
2025-11-22 19:28:29 -0600

    Update my-simple-widget.js

--- my-simple-widget.js
+++ my-simple-widget.js
@@ -2,16 +2,28 @@ class MyWidget extends HTMLElement {
   connectedCallback() {
     // We simply inject HTML directly into the tag
     this.innerHTML = `
+<style>   
+    quotelink{ 
+       background-color: #dcdcdc;
+  color: #444;
+  cursor: pointer;
+  padding: 2px;
+  border: none;
+  text-align: left;
+  outline: none;
+   width: 100%;
+	height: 20%;
+   margin-left: 1%;
+	 margin-right: 1%;
+	border: 5px outset #6A4E20;
+  }
+  </style>
       <div>
         <h3>My Links</h3>
         
-        <button onclick="window.location.href='https://google.com'">Go to Google</button>
+        <button class="quotelink"onclick="window.location.href='https://google.com'">Go to Google</button>
         
-        <a href="https://youtube.com">
-            <button>Go to YouTube</button>
-        </a>
-        
-        <a href="/contact">Contact Us</a>
+      
       </div>
     `;
   }
