2e441a780fa01d9a632843d21ffb3301ee4b2e21
Classic Quotes from Classic Books <62530007+CoderGirl-2@users.noreply.github.com>
2025-11-29 18:14:17 -0600

    Add 'Crime and Punishment' and reorder buttons

--- my-simple-widget.js
+++ my-simple-widget.js
@@ -31,7 +31,8 @@ class MyWidget extends HTMLElement {
         <h3>Directory of Quotes</h3>
         <button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/A Grief Observed'"><b>A Grief Observed, by C.S Lewis</b></button>
         <button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Brave%20New%20World'"><b>Brave New World, by Aldous Huxley</b></button>
-         <button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451'"><b>Fahrenheit 451, by Ray Bradbury</b></button>
+        <button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Crime and Punishment'"><b>Crime and Punishment, by Fyodor Dostoevsky</b></button>
+		 <button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Fahrenheit 451'"><b>Fahrenheit 451, by Ray Bradbury</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Heart of Darkness'"><b>Heart of Darkness, by Joseph Conrad</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Letters'"><b>Screwtape Letters, by C.S Lewis</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Screwtape Proposes a Toast'"><b>Screwtape Proposes a Toast, by C.S 
@@ -39,7 +40,9 @@ class MyWidget extends HTMLElement {
      	<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/The Abolition of Man'"><b>The Abolition of Man, by C.S Lewis</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/The Great Divorce'"><b>The Great Divorce, by C.S Lewis</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/The Gulag Archipelago'"><b>The Gulag Archipelago, by Alexander Solzhenitsyn</b></button>
+		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/The Republic'"><b>The Republic, by Plato</b></button>
 		<button class="quotelink"onclick="window.location.href='https://classic-wisdom-and-inspiration-quotes.vercel.app/Work and Days'"><b>Work and Days, by Hesiod</b></button>
+		
       </div>
     `;
   }
