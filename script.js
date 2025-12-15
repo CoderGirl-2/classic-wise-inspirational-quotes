a07a56d44888dce57832734962c3b2d13dcc0254
Classic Quotes from Classic Books <62530007+CoderGirl-2@users.noreply.github.com>
2025-12-14 20:30:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,18 +17,6 @@ function copyText(buttonElement) {
 
 
 
-function copyText(buttonElement) {
-    var quotebox = buttonElement.closest('.basic-box1');
-    
-    var textElement = quotebox.querySelector('.content-to-copy p');
-    
-    if (textElement) {
-        var textToCopy = textElement.innerText;
-        
-        navigator.clipboard.writeText(textToCopy)
-            
-    }
-}
 
 
 
